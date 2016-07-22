bb80c93e3408bf686a502cf7160626f42b4c3575
fliu <fliu@FA080621>
2016-07-22 10:21:20 +1000

    added a second datacontext in a separate project

---
+++ SecondContext/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondContext
+{
+    public class Teacher
+    {
+        public int TeacherID { get; set; }
+        public string FullName { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
